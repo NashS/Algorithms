93b0ed1f5dea2372af43848ebda29ff8addd01e1
NashS <nasseq@gmail.com>
2013-10-19 16:28:11 -0400

    Added Fast Algorithm

--- Assignment 3/Brute.java
+++ Assignment 3/Brute.java
@@ -1,9 +1,56 @@
+import java.util.Arrays;
+
 
 public class Brute {
+	private static final int MIN_POINTS_IN_LINE_SEGMENT = 4;
+	
 	public static void main(String[] args)
 	{
-        In in = new In(args[0]);      // input file
-        int N = in.readInt();   
+		StdDraw.setXscale(0, 32768);
+		StdDraw.setYscale(0, 32768);
 		
+        In in = new In(args[0]);      // input file
+        int N = in.readInt();
+        int x = 0;
+        int y = 0;
+        Point[] pointArray = new Point[N];
+        Point[] lineSegmentPoints = new Point[MIN_POINTS_IN_LINE_SEGMENT];
+        Point currPoint;
+        //Populate points array from point coordinates in input file
+        for(int i = 0;i < pointArray.length; i++)
+        {
+            x = in.readInt();
+            y = in.readInt();
+            currPoint = new Point(x,y);
+            pointArray[i] = currPoint;
+            currPoint.draw();     
+        }
+        for(int i = 0; i < pointArray.length-3; i++)
+        {
+        	for(int j = i+1; j < pointArray.length-2; j++)
+        	{
+        		for(int k = j+1; k < pointArray.length-1; k++)
+        		{
+        			for(int l = k+1; l < pointArray.length; l++)
+        			{
+        				if( (pointArray[i].slopeTo(pointArray[j]) == pointArray[i].slopeTo(pointArray[k])) && 
+        					(pointArray[i].slopeTo(pointArray[j]) == pointArray[i].slopeTo(pointArray[l])) )
+        				{
+        					lineSegmentPoints[0] = pointArray[i];
+        					lineSegmentPoints[1] = pointArray[j];
+        					lineSegmentPoints[2] = pointArray[k];
+        					lineSegmentPoints[3] = pointArray[l];
+        					Arrays.sort(lineSegmentPoints);
+        					lineSegmentPoints[0].drawTo(lineSegmentPoints[3]);
+        					StdOut.println(lineSegmentPoints[0].toString() + " -> " + 
+        								   lineSegmentPoints[1].toString() + " -> " + 	
+        								   lineSegmentPoints[2].toString() + " -> " + 
+        								   lineSegmentPoints[3].toString());
+        				}
+        			}
+        		}
+        	}
+        }
+        
 	}
 }
--- Assignment 3/Point.java
+++ Assignment 3/Point.java
@@ -1,4 +1,5 @@
 import java.util.Comparator;
+import java.util.Arrays;
 
 
 public class Point implements Comparable<Point> {
@@ -55,6 +56,11 @@ public class Point implements Comparable<Point> {
 				return Double.NEGATIVE_INFINITY;
 			return Double.POSITIVE_INFINITY;
 		}
+		else
+		{
+			if(dy == 0)
+				return 0;
+		}
 		return dy/dx;
 	}
 
@@ -83,7 +89,80 @@ public class Point implements Comparable<Point> {
     // unit test
     public static void main(String[] args) {
         /* YOUR CODE HERE */
-	
+    	Point origin = new Point(0,0);
+    	Point p1 = new Point(1,2);
+    	Point p2 = new Point(3,1);
+    	Point neg = new Point(-1,-1);
+    	Point degen = new Point(0,0);
+    	Point horz = new Point(2,0);
+    	Point horz2 = new Point(3,0);
+    	Point vert = new Point (0,2);
+    	Point vert2 = new Point (0,3);
+    	Point[] pointArray = new Point[9];
+    	pointArray[0] = origin;
+    	pointArray[1] = p1;
+    	pointArray[2] = p2;
+    	pointArray[3] = neg;
+    	pointArray[4] = degen;
+    	pointArray[5] = horz;
+    	pointArray[6] = vert;
+    	pointArray[7] = horz2;
+    	pointArray[8] = vert2;
+    	
+    	Arrays.sort(pointArray);
+    	
+    	
+    	StdOut.println("Origin: " + origin.toString());
+    	StdOut.println("p1: " + p1.toString());
+    	StdOut.println("p2: " + p2.toString());
+    	StdOut.println("neg: " + neg.toString());
+    	StdOut.println("degen: " + degen.toString());
+    	StdOut.println("horz: " + horz.toString());
+    	StdOut.println("vert: " + vert.toString());
+    	StdOut.println("horz2: " + horz2.toString());
+    	StdOut.println("vert2: " + vert2.toString());
+    	StdOut.println("");
+    	StdOut.println("Testing compareTo()");
+    	StdOut.println("");
+    	StdOut.println("Origin.compareTo(p1): "+ origin.compareTo(p1));
+    	StdOut.println("Origin.compareTo(degen): "+ origin.compareTo(degen));
+    	StdOut.println("Origin.compareTo(neg): "+ origin.compareTo(neg));
+    	StdOut.println("");
+    	StdOut.println("Testing slopeTo()");
+    	StdOut.println("");
+    	StdOut.println("Origin.slopeTo(p1): "+ origin.slopeTo(p1));
+    	StdOut.println("Origin.slopeTo(horz): "+ origin.slopeTo(horz));
+    	StdOut.println("Origin.slopeTo(degen): "+ origin.slopeTo(degen));
+    	StdOut.println("Origin.slopeTo(vert): "+ origin.slopeTo(vert));
+    	StdOut.println("Origin.slopeTo(neg): "+ origin.slopeTo(neg));
+    	StdOut.println("");
+    	StdOut.println("unsorted array: ");
+    	StdOut.println("");
+    	for(int i = 0;i<pointArray.length;i++)
+    		StdOut.println("pointArray[" + i + "]: " + pointArray[i]);
+    	StdOut.println("");
+    	StdOut.println("Testing natural ordering sort: ");
+    	StdOut.println("");
+    	Arrays.sort(pointArray);
+    	for(int i = 0;i<pointArray.length;i++)
+    		StdOut.println("pointArray[" + i + "]: " + pointArray[i]);
+    	pointArray[0] = origin;
+    	pointArray[1] = p1;
+    	pointArray[2] = p2;
+    	pointArray[3] = neg;
+    	pointArray[4] = degen;
+    	pointArray[5] = horz;
+    	pointArray[6] = vert;
+    	pointArray[7] = horz2;
+    	pointArray[8] = vert2;
+    	StdOut.println("");
+    	StdOut.println("Testing slope ordering(w.r.t. origin) sort: ");
+    	StdOut.println("");
+    	Arrays.sort(pointArray,origin.SLOPE_ORDER);
+    	for(int i = 0;i<pointArray.length;i++)
+    		StdOut.println("pointArray[" + i + "]: " + pointArray[i]);
+    	StdOut.println("");
+    	StdOut.println("");
     }
 	
 }
